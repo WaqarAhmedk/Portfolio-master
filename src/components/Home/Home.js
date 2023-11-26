f3fb0eb6c4a75e66de39f3297934d9f9bff36aa2
Waqar Ahmed <wlwaqarah0@gmail.com>
2023-11-26 14:55:30 +0500

    x

---
+++ src/components/Home/Home.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import homeLogo from "../../Assets/home-main.svg";
+import Particle from "../Particle";
+import Home2 from "./Home2";
+import Type from "./Type";
+
+function Home() {
+  return (
+    <section>
+      <Container fluid className="home-section" id="home">
+        <Particle />
+        <Container className="home-content">
+          <Row>
+            <Col md={7} className="home-header">
+              <h1 style={{ paddingBottom: 15 }} className="heading">
+                Hi There!{" "}
+
+              </h1>
+
+              <h1 className="heading-name">
+
+                <strong className="main-name">WAQAR AHMED</strong>
+
+              </h1>
+              <h1 style={{ paddingBottom: 15, textAlign: 'center' }} className="heading">
+                A Passionate
+
+              </h1>
+
+              <div style={{ textAlign: "left", display: 'flex', justifyContent: 'center' }}>
+                <Type />
+              </div>
+            </Col>
+
+            <Col md={5} style={{ paddingBottom: 20 }}>
+              <img
+                src={homeLogo}
+                alt="home pic"
+                className="img-fluid"
+                style={{ maxHeight: "450px" }}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+      <Home2 />
+    </section>
+  );
+}
+
+export default Home;
---
+++ src/components/Home/Home2.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+              Hello, I'm <b className="purple">Waqar Ahmed</b>, a skilled  <b className="purple">MERN developer </b>, with a passion for crafting innovative web applications.
+              <br />
+
+
+
+            </p>
+            I've honed my expertise in building robust and dynamic web solutions. My journey in web development has taken me through the intricacies of the MERN (MongoDB, Express, React, and Node.js) stack, where I've successfully brought countless projects to life. In addition to my MERN proficiency, I've also embraced the power of Next.js to create seamless, high-performance web experiences. My commitment to clean code, user-centric design, and staying current with the latest technologies drives me to deliver solutions that not only meet but exceed expectations. I'm excited to continue pushing the boundaries of what's possible in web development and collaborating on projects that make a difference.
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/waqarahmedk"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/waqarahmedk"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
